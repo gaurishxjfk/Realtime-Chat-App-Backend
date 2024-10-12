92dd2d7b19e67cbeeb12867208ac6168af819ed3
GaurishN <gaurish.naik@raweng.com>
2024-10-12 22:28:11 +0530

    added cors url

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@ import cors from "cors";
 import { app, server } from "./socket/socket";
 
 dotenv.config();
-
+const FRONTEND_URL: string = process.env.FRONTEND_URL ?? "http://localhost:5173";
 
 app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: FRONTEND_URL, 
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
