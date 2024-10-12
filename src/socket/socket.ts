7d189c0b7a27186b058c42b78ca665745b46682f
GaurishN <gaurish.naik@raweng.com>
2024-10-12 22:46:46 +0530

    added cors url to socket

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -5,10 +5,11 @@ import express from "express";
 const app = express();
 
 const server = http.createServer(app);
+const FRONTEND_URL: string = process.env.FRONTEND_URL ?? "http://localhost:5173";
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [FRONTEND_URL],
     methods: ["GET", "POST"],
   },
 });
